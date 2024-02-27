8e4f6175107c56679b1f64b8f209a4f14d999dcd
Dilan Demirel <67210120+dilandemirel@users.noreply.github.com>
2024-02-27 16:11:03 +0300

    Add files via upload

---
+++ Assets/Scripts/MissileController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+
+
+    private void FixedUpdate()
+    {
+        var direction = PlayerController.Instance.transform.position - transform.position;
+        direction = direction.normalized; // uzunluk farketmeksizin 0 ile 1 arasinda
+
+        var rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rigidbody.velocity = transform.up * (moveSpeed * Time.deltaTime);
+        rigidbody.angularVelocity = -rotateAmount * (rotateSpeed * Time.deltaTime);
+    }
+}
+
